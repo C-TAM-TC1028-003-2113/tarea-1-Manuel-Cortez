bd02818bbea9313d89284bae40641896846217b4
Manuel-Cortez <89654875+Manuel-Cortez@users.noreply.github.com>
2021-08-29 17:18:19 -0500

    Update exercise.py

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,7 +1,12 @@
 def main():
     # escribe tu código abajo de esta línea
     pass
-
+n=int(input("Dame la cantidad de juegos nuevos: "))
+u=int(input("Dame la cantidad de juegos usados: "))
+juegosusados= u*350
+juegosnuevos= n*1000
+totaldecompra=juegosnuevos+juegosusados
+print("El total de la compra es: ",totaldecompra)
 
 if __name__ == '__main__':
     main()
