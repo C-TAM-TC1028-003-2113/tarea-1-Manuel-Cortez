70a1f5340bd1a29d7e38b7c27931025c3af34557
Manuel-Cortez <89654875+Manuel-Cortez@users.noreply.github.com>
2021-08-29 17:14:27 -0500

    Update exercise.py

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,7 +1,16 @@
 def main():
     # escribe tu código abajo de esta línea
     pass
+msj=int(input("Dame el número de mensajes: "))
+m=float(input("Dame el número de megas: "))
+minutos=int(input("Dame el número de minutos: "))
 
+mensajes=0.80*msj
+megas=0.80*m
+minu=0.80*minutos
+
+costomensual=mensajes+megas+minu
+print("El costo mensual es: ",costomensual)
 
 if __name__ == '__main__':
     main()
