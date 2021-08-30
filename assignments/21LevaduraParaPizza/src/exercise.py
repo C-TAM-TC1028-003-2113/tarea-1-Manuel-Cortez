f5a3f9abd8fdac9f2d8f5eef694e66baa59f99b9
Manuel-Cortez <89654875+Manuel-Cortez@users.noreply.github.com>
2021-08-29 21:50:25 -0500

    Update exercise.py

--- assignments/21LevaduraParaPizza/src/exercise.py
+++ assignments/21LevaduraParaPizza/src/exercise.py
@@ -1,7 +1,7 @@
 def main():
     # escribe tu código abajo de esta línea
     pass
-gramosharina= float(input("Dame la harina en gramo
+gramosharina= float(input("Dame la harina en gramos: ))
 kiloengramos=50
 mediokiloengramos=25
 gramoslevadura= kiloengramos+mediokiloengramos
