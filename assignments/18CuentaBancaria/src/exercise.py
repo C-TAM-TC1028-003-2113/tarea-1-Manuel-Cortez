d75290cbd07543998fdbbb027569647baa841b9e
Manuel-Cortez <89654875+Manuel-Cortez@users.noreply.github.com>
2021-08-31 12:26:30 -0500

    Update exercise.py

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -1,9 +1,9 @@
 def main():
  # escribe tu código abajo de esta línea
    saldomes= float(input("Dame el saldo del mes anterior: "))
-   ingresos= float(input("Dame el ingreso: "))
-   egresos= float(input("Dame el egreso: "))
-   numcheques= int(input("Dame el numero de cheques: "))
+   ingresos= float(input("Dame los ingresos: "))
+   egresos= float(input("Dame los egresos: "))
+   numcheques= int(input("Dame el número de cheques: "))
    cheque= numcheques*13
    descuento= 9.46875
    saldofinal= (saldomes-cheque)+(ingresos-egresos)
